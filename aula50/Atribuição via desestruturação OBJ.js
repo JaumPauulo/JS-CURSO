0f2054f1d932cae88b95fc444eb271f8c2031111
João Paulo <joaopaulodasilva506@gmail.com>
2024-05-17 11:16:55 -0300

    Aula de Atribuição via desestruturação Objeto

---
+++ aula50/Atribuição via desestruturação OBJ.js
@@ -0,0 +1,12 @@
+const  pessoa = {
+    nome: 'joao',
+    sobrenome: 'silva',
+    idade: 50,
+    endereco: {
+        rua: 'maria de fatima',
+        numero: 127
+    }
+};
+
+const nome = pessoa.nome; //atribuição normal
+ console.log(pessoa.nome)
